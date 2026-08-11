5378edd8e25b80b19e4b73270774089a9a45f4cd
jefersonbojorquez764-cloud <jefersonbojorquez764@gmail.com>
2026-08-11 11:18:06 -0700

    1

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 ScrollReveal().reveal('.container' );
 ScrollReveal().reveal('.main', { delay: 500, duration: 1000 });
 ScrollReveal().reveal('.Country', { delay: 500, duration: 1000 });
+
+async function myFunction() {
+    const response = await fetch(
+        "appaANRWHYEpuYpGy",
+        {
+            headers: {
+                Authorization: "Bearer pat09kjus1llkT9Jg.a54e36b7d79bf51c6d862abdf023276c03a8120cf99a64885bfe2332154e0e06"
+            },
+            method: "GET"
+        }
+    );
+
+    const data = await response.json();
+
+    console.log(data);
+    console.log(data.records);
+
+    console.log(data.records[0]);
+    console.log(data.records[0].fields["Name"]);
+}
